0ab532bf11400beb4e6c3b7bd82816e133838c2f
Osama Haddad <57003889+haddadosama@users.noreply.github.com>
2022-03-08 10:48:04 -0500

    Update .custom_git.sh

--- .custom_git.sh
+++ .custom_git.sh
@@ -5,7 +5,7 @@ function git-commit() {
 }
 
 function git-clean() {
-		git checkout dev
+		git checkout main
 		git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
 		git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '{sub("refs/heads/", "", $1); print $1}'); do git checkout $branch; git pull; done
 }
